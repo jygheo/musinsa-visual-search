7e1e9640d0ecb0d2dac933a4132dc2ca22c5ed8e
jygheo <ygheo4@gmail.com>
2025-06-05 15:47:22 -0400

    Fix postgres connection

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,3 +1,14 @@
 import psycopg2
+from pgvector.psycopg2 import register_vector
+import os
 
-connection = psycopg2.connect(database="dbname", user="username", password="pass", host="hostname", port=5432)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        database="fashion_search",
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host="localhost"
+    )
+    register_vector(conn)
+    return conn
