f06cd68a303ec717e43c81bc65898d89355553e8
jygheo <ygheo4@gmail.com>
2025-06-05 19:40:12 -0400

    Remove user_agents.py from tracking

--- config/user_agents.py
+++
@@ -1,8 +0,0 @@
-
-USER_AGENTS = ['Mozilla/5.0 (compatible; FacebookBot/1.0; +https://developers.facebook.com/docs/sharing/webmasters/facebookbot/)',
-               'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-               'Yeti/1.0 (NHN Corp.; http://help.naver.com/robots/)',
-               'Yeti/1.0 (+http://help.naver.com/robots/)',
-               'Mozilla/5.0 (compatible; MSIE or Firefox mutant; not on Windows server;) Daumoa-image/1.0',
-               'Mozilla/5.0 (compatible; MSIE or Firefox mutant; not on Windows server;) Daumoa 4.0',
-               'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)']
