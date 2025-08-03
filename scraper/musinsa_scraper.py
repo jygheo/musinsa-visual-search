d324758d0af98129b81f79dcc173a3c23010baa6
jygheo <ygheo4@gmail.com>
2025-08-03 17:16:05 -0400

    refactor scraper

--- scraper/musinsa_scraper.py
+++ scraper/musinsa_scraper.py
@@ -5,8 +5,8 @@ import re
 import json
 from config.user_agents import USER_AGENTS
 from backend.database.db import get_db_connection
+import time
 
-# TODO: use https://global.musinsa.com/us/category/clothing?page=1&sortCode=NEW (avoid duplicates)
 
 def get_num_pages():
     url = f"https://global.musinsa.com/us/category/clothing?page=1"
@@ -26,7 +26,8 @@ def get_num_pages():
             data = json.loads(json_str)
             return (data["totalCount"]/100 + 1)
 
-def get_page_info(page_num=1):
+
+def get_page_info(cur, conn, page_num=1):
     url = f"https://global.musinsa.com/us/category/clothing?page={page_num}"
     response = requests.get(url, headers={
         'User-Agent': random.choice(USER_AGENTS)})
@@ -46,12 +47,10 @@ def get_page_info(page_num=1):
                 data = json.loads(json_str)
                 goods_info_list = data["goodsInfoList"]
                 for goods_dict in goods_info_list:
-                    prod_num, prod_name, brand_name, price = goods_dict["goodsNo"], goods_dict["goodsName"], goods_dict["brandName"], goods_dict["price"]
+                    prod_num, prod_name, brand_name, price = goods_dict["goodsNo"], goods_dict[
+                        "goodsName"], goods_dict["brandName"], goods_dict["price"]
                     image_url = "https:"+goods_dict["imageUrl"]
                     prod_url = f"https://global.musinsa.com/us/goods/{goods_dict['goodsNo']}"
-
-                    conn = get_db_connection()
-                    cur = conn.cursor()
                     cur.execute("""
                         INSERT INTO products (
                             prod_num, prod_name, brand_name, price,
@@ -60,17 +59,28 @@ def get_page_info(page_num=1):
                         ON CONFLICT (prod_num) DO UPDATE SET
                             price = EXCLUDED.price
                     """, (prod_num, prod_name, brand_name, price, image_url, prod_url))
-                    conn.commit()
-                    conn.close()
+                conn.commit()
             except Exception as e:
                 print(f"{e} on page {page_num}.")
+                conn.rollback()
         else:
             print(f"Could not find goodsList JSON on page {page_num}.")
     else:
         print(f"No relevant <script> tag found on page {page_num}.")
 
-MAX_PAGES = 1
 
 if __name__ == "__main__":
-    get_page_info()
-
+    MAX_PAGES = 100
+    conn = get_db_connection()
+    cur = conn.cursor()
+    for i in range(1, MAX_PAGES+1):
+        print(i)
+        # start_time = time.perf_counter()
+        get_page_info(cur, conn, i)
+        # end_time = time.perf_counter()
+        # elapsed_time = end_time - start_time
+        # print(f"Elapsed time: {elapsed_time:.4f} seconds")
+        # print("sleeping")
+        time.sleep(random.randint(5,13))
+    cur.close()
+    conn.close()
