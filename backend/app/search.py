22713ec1f26d6f11aee45777be478a70be5fcce9
jygheo <ygheo4@gmail.com>
2026-07-27 14:44:23 -0400

    update search to new schema

--- backend/app/search.py
+++ backend/app/search.py
@@ -2,24 +2,41 @@ import time
 import numpy as np
 from app.db import get_db_connection
 
-
-def find_sim_products(query_embedding: np.ndarray, top_k: int = 20, ef_search:int = 200) -> list[dict]:
+def find_sim_products(query_embedding: np.ndarray, top_k: int = 20, ef_search: int = 200) -> list[dict]:
     start = time.perf_counter()
     conn = get_db_connection()
     cur = conn.cursor()
+    
     cur.execute("SET hnsw.ef_search = %s", (ef_search,))
+    
     query = """
-        SELECT p.prod_num, p.name, p.brand, p.price, p.image_url, p.url,
-               (SELECT 1 - (pg.image_embedding <=> %s) AS similarity FROM product_garments pg WHERE p.id=pg.id). 
-        FROM products p
-        ORDER BY image_embedding <=> %s
+        SELECT 
+            p.prod_num, 
+            p.name, 
+            p.brand, 
+            p.price, 
+            p.image_url, 
+            p.url,
+            1 - (pg.embedding <=> %s::vector) AS similarity,
+            pg.id AS garment_id,
+            pg.bbox,
+            pg.polygon,
+            pg.category,
+            pg.is_primary
+        FROM product_garments pg
+        JOIN products p ON pg.product_id = p.id
+        ORDER BY pg.embedding <=> %s::vector
         LIMIT %s
-    """  #TODO fix 
-    cur.execute(query, [query_embedding, query_embedding, top_k])
+    """
+    
+    cur.execute(query, [query_embedding.tolist(), query_embedding.tolist(), top_k])
     res = cur.fetchall()
+    
     cur.close()
     conn.close()
+    
     print(f"Search runtime: {(time.perf_counter() - start) * 1000:.1f} ms")
+    
     return [
         {
             "prod_num": row[0],
@@ -29,6 +46,11 @@ def find_sim_products(query_embedding: np.ndarray, top_k: int = 20, ef_search:in
             "image_url": row[4],
             "prod_url": row[5],
             "similarity": row[6],
+            "garment_id": row[7],
+            "bbox": row[8],
+            "polygon": row[9],
+            "category": row[10],
+            "is_primary": row[11]
         }
         for row in res
     ]
